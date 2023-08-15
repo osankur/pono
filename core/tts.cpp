54eafa5ea15e31419cc418b532b4f0f8ee838427
Ocan Sankur <ocan.sankur@cnrs.fr>
2023-08-15 22:22:33 +0200

    tts

---
+++ core/tts.cpp
@@ -0,0 +1,38 @@
+#include "smt-switch/term.h"
+#include "core/tts.h"
+using namespace smt;
+namespace pono{
+    const std::string TimedTransitionSystem::GLOBAL_CLOCK_VAR_NAME = "#global_time";
+    
+    void TimedTransitionSystem::initialize(){
+        // Register nonclock and clock variables
+        for (const auto & sv : statevars()) {
+            switch(sv->get_sort()->get_sort_kind()){
+                case smt::SortKind::REAL:
+                clock_vars_.insert(sv);
+                break;
+                default:
+                nonclock_vars_.insert(sv);
+                // throw PonoException("The timed automaton solver only accepts bool and real sorts. Got " + sv->get_sort()->to_string());
+            }
+        }
+
+        smt::Sort realsort = solver_->make_sort(REAL);
+        Term delta = make_statevar(GLOBAL_CLOCK_VAR_NAME, realsort);
+
+        // delta = 0
+        Term delta_init = solver_->make_term(Equal, delta, solver_->make_term((int64_t) 0, realsort));
+        set_init(solver_->make_term(And, original_ts_.init(), delta_init));
+
+        // time_elapse = delta <= delta' /\\ v' = v (for all nonclock v)
+        Term time_elapse = solver_->make_term(Le, delta, next(delta));
+        for(auto & v : nonclock_vars_){
+            time_elapse = solver_->make_term(And, time_elapse, 
+                solver_->make_term(Equal, next(v), v));
+        }
+
+        // discrete = delta' = delta /\ trans
+        Term discrete = solver_->make_term(Equal, delta, next(delta));
+        discrete = solver_->make_term(And, discrete, trans());
+    }
+} 
---
+++ core/tts.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "core/ts.h"
+#include "core/rts.h"
+
+namespace pono{
+class TimedTransitionSystem : public RelationalTransitionSystem {
+    public:
+    TimedTransitionSystem(const smt::SmtSolver & s, const TransitionSystem & ts) : RelationalTransitionSystem(s), original_ts_(ts), solver_(s){
+        initialize();
+    }
+    static const std::string GLOBAL_CLOCK_VAR_NAME;
+
+    const smt::UnorderedTermSet & nonclock_vars() {
+        return nonclock_vars_;
+    }
+    const smt::UnorderedTermSet & clock_vars() {
+        return clock_vars_;
+    }
+
+    private:
+    void initialize();
+    const TransitionSystem & original_ts_;
+    const smt::SmtSolver & solver_;
+    smt::UnorderedTermSet nonclock_vars_;
+    smt::UnorderedTermSet clock_vars_;
+};
+
+}
